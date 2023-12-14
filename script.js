c3599e838a3578242a96522a5cedac60537b7ee3
shad anjum <92822708+shad-pythondev123@users.noreply.github.com>
2023-12-14 15:59:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let ans= null;
+	let myMap= new Map();
+	let n= str.length;
+	for(let i=0; i<n; i++){
+		if(myMap.has(str[i])){
+			myMap.set(str[i],myMap.get(str[i])+1);
+		}else{
+			myMap.set(str[i],1);
+		}
+	}
+	for(const values of myMap.values()){
+		if(value<==1) ans= value;
+	}
+	return ans;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
